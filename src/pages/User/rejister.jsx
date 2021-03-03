02cea29151f64d473a03b218efd5f36e9c0b70d6
tyrone <1161763410@qq.com>
2021-03-03 22:53:10 +0800

    2021.03.03 用户注册修改

--- src/pages/User/rejister.jsx
+++ src/pages/User/rejister.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import { Button, Input } from 'antd';
+import { Button, Input, Form, message } from 'antd';
 import '@/style/user.less';
+import { UserOutlined, PhoneOutlined, MailOutlined } from '@ant-design/icons';
 
 export default class user extends Component {
 	state = {
 		show: false,
 	};
-	go2Login() {
+	onFinish = (userInfo) => {
+		const { user_name, user_pwd } = userInfo;
+		message.success('注册成功');
 		this.props.history.push('/login');
-	}
+	};
+
+	onFinishFailed = (errorInfo) => {
+		message.error('注册失败，请重试！');
+	};
 
 	render() {
 		return (
@@ -29,35 +36,62 @@ export default class user extends Component {
 						<div style={{ fontSize: '34px', margin: '20px 0' }}>注册</div>
 					</div>
 					<div className="user-form">
-						<div className="user-form-item">
+						<Form
+							name="login"
+							initialValues={{ remember: true }}
+							onFinish={(userInfo) => this.onFinish(userInfo)}
+							onFinishFailed={() => this.onFinishFailed()}
+						>
 							<p className="user-form-item-label">个人昵称</p>
-							<Input placeholder="请输入昵称" />
-						</div>
-						<div className="user-form-item">
-							<p className="user-form-item-label">电话号码</p>
-							<Input placeholder="请输入电话号码" />
-						</div>
-						<div className="user-form-item">
-							<p className="user-form-item-label">邮箱设置</p>
-							<Input placeholder="请输入邮箱" />
-						</div>
-						<div className="user-form-item">
-							<p className="user-form-item-label">设置密码</p>
-							<Input placeholder="请输入你的密码" />
-						</div>
-						<div className="user-form-item">
-							<p className="user-form-item-label">确认密码</p>
-							<Input placeholder="请再次输入你的密码" />
-						</div>
-						<div className="user-form-item">
-							<Button
-								type="primary"
-								className="user-btn"
-								onClick={() => this.go2Login()}
+							<Form.Item
+								name="user_name"
+								className="user-form-item"
+								rules={[{ required: true, message: '请输入昵称！' }]}
 							>
-								注册
-							</Button>
-						</div>
+								<Input placeholder="请输入昵称" suffix={<UserOutlined />} />
+							</Form.Item>
+							<p className="user-form-item-label">电话号码</p>
+							<Form.Item
+								name="user_phone"
+								className="user-form-item"
+								rules={[{ required: true, message: '请输入电话号码！' }]}
+							>
+								<Input
+									placeholder="请输入电话号码"
+									suffix={<PhoneOutlined />}
+								/>
+							</Form.Item>
+							<p className="user-form-item-label">邮箱设置</p>
+							<Form.Item
+								name="user_email"
+								className="user-form-item"
+								rules={[{ required: true, message: '请输入邮箱！' }]}
+							>
+								<Input placeholder="请输入邮箱" suffix={<MailOutlined />} />
+							</Form.Item>
+							<p className="user-form-item-label">设置密码</p>
+							<Form.Item
+								name="user_pwd"
+								className="user-form-item"
+								rules={[{ required: true, message: '请输入密码！' }]}
+							>
+								<Input.Password placeholder="请输入密码" />
+							</Form.Item>
+							<p className="user-form-item-label">确认密码</p>
+							<Form.Item
+								name="user_pwd2"
+								className="user-form-item"
+								rules={[{ required: true, message: '请再次输入密码！' }]}
+							>
+								<Input.Password placeholder="请再次输入密码" />
+							</Form.Item>
+
+							<Form.Item className="user-form-item">
+								<Button type="primary" htmlType="submit" className="user-btn">
+									登陆
+								</Button>
+							</Form.Item>
+						</Form>
 					</div>
 					<div className="user-form-bottom">
 						<div onClick={() => this.props.history.push('/login')}>
